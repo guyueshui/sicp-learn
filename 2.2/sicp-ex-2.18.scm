9ac62d852a48dc9a7cac1e08304cdf74e9a7e7d7
guyueshui <guyueshui002@gmail.com>
2019-05-23 11:19:51 +0800

    correct my-reverse

--- 2.2/sicp-ex-2.18.scm
+++ 2.2/sicp-ex-2.18.scm
@@ -5,7 +5,7 @@
 (define (my-reverse lst)
   (if (null? lst)
       '()
-      (append (reverse (cdr lst))
+      (append (my-reverse (cdr lst))
             (list (car lst)))))
 
 (my-reverse (list 1 4 9 16 25))
